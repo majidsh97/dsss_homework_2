b15c080682ccae50a6c24906048d92ea621cbdea
majidsh97 <sharghi.majid@gmail.com>
2023-11-13 17:50:13 +0100

    v3

---
+++ math_quiz/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/majidsh97/dsss_homework_2',
+)
