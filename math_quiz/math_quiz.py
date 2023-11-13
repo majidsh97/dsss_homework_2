fb7e58d4cc9ebc3991acb83a319723d3df4424c5
majidsh97 <sharghi.majid@gmail.com>
2023-11-13 16:22:53 +0100

    v1

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,35 +1,72 @@
 import random
 
 
-def function_A(min, max):
+def get_random_num(min, max):
     """
+    this function gets two numbers and returns one random number
+    :param
+    `min`= minimum number,
+    `max`= maximuim number
+    :return
     Random integer.
     """
-    return random.randint(min, max)
+    #try:
+    r = random.randint(min, max)
+    #except:
+        #r = random.randint(int(min), int(max))
+    return r
 
 
-def function_B():
+def get_random_operation():
+    
+    """
+    @param: no Args
+    :)
+    ------------------------------------
+    @return: a random char
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
+def operation(n1, n2, o):
+    """
+    
+    do the operation
+    """
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    if o == '+': a = n1 + n2
+    elif o == '-': a = n1 - n2
     else: a = n1 * n2
     return p, a
 
 def math_quiz():
+    """
+    Executes a math quiz game by presenting math problems and evaluating user answers.
+
+    This function runs a simple math quiz game. It prompts the user with math problems
+    and evaluates the user's input against the correct answer. The user's score is calculated
+    based on correct answers.
+
+    The user is presented with a set number of questions determined by the value of `t_q`.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+
+
     s = 0
-    t_q = 3.14159265359
+    t_q = 3 #3.14159265359
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = get_random_num(1, 10); n2 = get_random_num(1, 5); o = get_random_operation()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = operation(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
@@ -44,3 +81,4 @@ def math_quiz():
 
 if __name__ == "__main__":
     math_quiz()
+    
